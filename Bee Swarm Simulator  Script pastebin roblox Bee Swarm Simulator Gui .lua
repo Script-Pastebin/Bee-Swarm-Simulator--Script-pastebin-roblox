98d7b2ed6270aee6bb7dedcf24a684efd825efc5
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:31:50 +0100

    commit zero

---
+++ Bee Swarm Simulator  Script pastebin roblox Bee Swarm Simulator Gui .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:GetObjects("rbxassetid://4384103988")[0X1].Source)("Pepsi Swarm")
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+This script has: 1) Auto-Farm, 2) Teleports, 3) Assistant to kill bosses, 4) Auto-Buy, 5) Auto-find items and more !!
+]]
